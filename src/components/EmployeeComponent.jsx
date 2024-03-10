571902afe29698e90a354a5e8b9d6b0e29279fb3
Eren Gaygusuz <gaygusuzeren@gmail.com>
2024-03-10 22:36:14 +0300

    Employee Details are Listed on Update Employee Page

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react'
-import { createEmployee } from '../services/EmployeeService'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { createEmployee, getEmployee } from '../services/EmployeeService'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const EmployeeComponent = () => {
 
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [email, setEmail] = useState('')
+
+  const {id} = useParams();
 
   const [errors, setErrors] = useState({
     firstName: '',
@@ -15,6 +17,18 @@ const EmployeeComponent = () => {
   })
 
   const navigator = useNavigate();
+
+  useEffect(() => {
+    if(id){
+      getEmployee(id).then((response) => {
+        setFirstName(response.data.firstName)
+        setLastName(response.data.lastName)
+        setEmail(response.data.email)
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+  }, [id])
 
   function saveEmployee(e){
     e.preventDefault();
@@ -62,12 +76,20 @@ const EmployeeComponent = () => {
     return valid;
   }
 
+  function pageTitle(){
+    if(id){
+      return <h2 className='text-center'>Update Employee</h2>
+    }else {
+      return <h2 className='text-center'>Add Employee</h2>
+    }
+  }
+
   return (
     <div className='container'>
 
       <div className='row mt-5'>
         <div className='card col-md-6 offset-md-3'>
-          <h2 className='text-center'>Add Employee</h2>
+          {pageTitle()}
           <div className='card-body'>
             <form>
                 <div className='form-group mb-2'>
