65ca692d62481d2458b0710438a13405e3f42449
erengaygusuz <gaygusuzeren@gmail.com>
2024-03-10 13:33:37 +0300

    react-router-dom Package is Added and Route is Configured for Root and Employees

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import './App.css'
-import FooterComponent from './components/FooterComponent'
-import HeaderComponent from './components/HeaderComponent'
-import ListEmployeeComponent from './components/ListEmployeeComponent'
+import "./App.css";
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-      <HeaderComponent/>
-      <ListEmployeeComponent/>
-      <FooterComponent/>
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          <Route path="/" element = { <ListEmployeeComponent/> }></Route>
+          <Route path="/elements" element = { <ListEmployeeComponent/> }></Route>
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
