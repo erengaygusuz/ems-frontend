08c1dbf31b2fc1e01f43d8c17a52ddaeae2edd19
erengaygusuz <gaygusuzeren@gmail.com>
2024-03-10 12:50:22 +0300

    Frontend Project Files are Added

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,5 @@
+function HelloWorld(){
+    return <h1 className="text-center">Title</h1>
+}
+
+export default HelloWorld;
---
+++ src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,47 @@
+import React, {useEffect, useState} from "react";
+import { listEmployees } from '../services/EmployeeService'
+
+const ListEmployeeComponent = () => {
+
+  // state variable name, state update function name
+  const [employees, setEmployees] = useState([])
+
+  // callback function, dependency list 
+  useEffect(() => {
+    listEmployees().then((response) => {
+      setEmployees(response.data)
+    }).catch(error => {
+      console.error(error)
+    })
+  }, [])
+
+  return(  
+  <div className="container">
+    <h2 className="text-center">List of Employees</h2>
+    <table className="table table-striped table-bordered">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>First Name</th>
+          <th>last Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+            employees.map(employee => 
+                <tr key={employee.id}>
+                    <td>{employee.id}</td>
+                    <td>{employee.firstName}</td>
+                    <td>{employee.lastName}</td>
+                    <td>{employee.email}</td>
+                </tr>
+            )
+        }
+      </tbody>
+    </table>
+  </div>
+  )
+};
+
+export default ListEmployeeComponent;
