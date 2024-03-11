9af63eab6c274c8995e42a5acdbe927794a2ee33
Eren Gaygusuz <gaygusuzeren@gmail.com>
2024-03-11 11:14:39 +0300

    Changing Base Url for Deployment

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const REST_API_BASE_URL = 'http://localhost:8080/api/employees';
+const REST_API_BASE_URL = 'http://16.170.247.6:8080/api/employees';
 
 export const listEmployees = () => axios.get(REST_API_BASE_URL);
 
