d9a38c880613207bfa8abf0934a449ed280fe9ad
erengaygusuz <gaygusuzeren@gmail.com>
2024-03-25 14:09:31 +0300

    Dummy Department Data is Listed and Department Table is Added

--- src/components/ListDepartmentComponent.jsx
+++ src/components/ListDepartmentComponent.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ListDepartmentComponent = () => {
+
+  let dummyData = [
+    {
+      "id": "1",
+      "name": "Department 1",
+      "description": "Description 1"
+    },
+    {
+      "id": "2",
+      "name": "Department 2",
+      "description": "Description 2"
+    }
+  ]
+
+  const [departments, setDepartments] = useState(dummyData);
+
   return (
-    <div>ListDepartmentComponent</div>
+    <div className='container'>
+      <h2 className='text-center'>List of Departments</h2>
+      <table className='table table-striped table-bordered table-responsive mt-2'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+          departments.map(department => 
+            <tr key={department.id}>
+              <td>{department.id}</td>
+              <td>{department.name}</td>
+              <td>{department.description}</td>
+            </tr>
+            )
+        }
+        </tbody>
+      </table>
+    </div>
   )
 }
 
