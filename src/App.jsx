195f9303ba8e591d4c2ea28887ec1ae105211b93
erengaygusuz <gaygusuzeren@gmail.com>
2024-03-25 13:52:47 +0300

    ListDepartmentComponent is Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import FooterComponent from "./components/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import ListDepartmentComponent from "./components/ListDepartmentComponent";
 import EmployeeComponent from "./components/EmployeeComponent";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -15,6 +16,7 @@ function App() {
           <Route path="/employees" element = { <ListEmployeeComponent/> }></Route>
           <Route path="/add-employee" element = { <EmployeeComponent/> }></Route>
           <Route path="/edit-employee/:id" element = { <EmployeeComponent/> }></Route>
+          <Route path="/departments" element = { <ListDepartmentComponent/> }></Route>
         </Routes>
         <FooterComponent />
       </BrowserRouter>
